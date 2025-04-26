3be6381bf6253877ffba923fa7f36af39ab33147
deluge <1870896906@qq.com>
2025-04-26 22:36:57 +0800

    根据文件夹添加命名空间

---
+++ OpenGL/include/component/readFile/file.h
@@ -0,0 +1,22 @@
+#ifndef DELUGE_FILE
+#define DELUGE_FILE
+namespace deluge_read_file
+{
+    class File
+    {
+    private:
+        /* data */
+    public:
+        File(/* args */);
+        ~File();
+    };
+    
+    File::File(/* args */)
+    {
+    }
+    
+    File::~File()
+    {
+    }
+} // namespace deluge_read_file
+#endif 
---
+++ OpenGL/include/component/readFile/read.h
@@ -0,0 +1,6 @@
+#ifndef DELUGE_READ
+#define DELUGE_READ
+namespace deluge_read_file
+{
+} // namespace deluge_read_file
+#endif
